0b89d45940ec0d5977f676cdb270793deb18caa5
DANIA SESTAN <contact@daniasestan.com>
2019-03-04 11:05:59 -0700

    Load Bootstrap Data on startup. Closes #14

--- pet-clinic-data/src/main/java/com/daniasestan/petclinic/services/map/PetServiceMap.java
+++ pet-clinic-data/src/main/java/com/daniasestan/petclinic/services/map/PetServiceMap.java
@@ -1,11 +1,11 @@
 package com.daniasestan.petclinic.services.map;
 
 import com.daniasestan.petclinic.model.Pet;
-import com.daniasestan.petclinic.services.CrudService;
+import com.daniasestan.petclinic.services.PetService;
 
 import java.util.Set;
 
-public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements PetService {
     @Override
     public Set<Pet> findAll() {
         return super.findAll();
--- pet-clinic-data/src/main/java/com/daniasestan/petclinic/services/map/VetServiceMap.java
+++ pet-clinic-data/src/main/java/com/daniasestan/petclinic/services/map/VetServiceMap.java
@@ -1,11 +1,11 @@
 package com.daniasestan.petclinic.services.map;
 
 import com.daniasestan.petclinic.model.Vet;
-import com.daniasestan.petclinic.services.CrudService;
+import com.daniasestan.petclinic.services.VetService;
 
 import java.util.Set;
 
-public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
     @Override
     public Set<Vet> findAll() {
         return super.findAll();
@@ -29,5 +29,10 @@ public class VetServiceMap extends AbstractMapService<Vet, Long> implements Crud
     @Override
     public Vet findById(Long id) {
         return super.findById(id);
+    }
+
+    @Override
+    public Vet findByLastName(String lastName) {
+        return null;
     }
 }
