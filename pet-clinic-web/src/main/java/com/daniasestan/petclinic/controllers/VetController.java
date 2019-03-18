caa74fdb7c8c95757f4d7386b187e1b6b5a446d3
DANIA SESTAN <contact@daniasestan.com>
2019-03-17 18:43:22 -0600

    Added Vet List and updated controller. Closes #15

--- pet-clinic-web/src/main/java/com/daniasestan/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/daniasestan/petclinic/controllers/VetController.java
@@ -1,41 +1,21 @@
 package com.daniasestan.petclinic.controllers;
 
+import com.daniasestan.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
-    public String listVets(){
+    public String listVets(Model model){
+        model.addAttribute("vets", vetService.findAll());
         return "/vets/index";
     }
 }
-
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//
-//@Controller
-//public class VetController {
-//
-////    private final VetService vetService;
-////
-////    public VetController(VetService vetService) {
-////        this.vetService = vetService;
-////    }
-//
-//    @RequestMapping({"/vets", "/vets/index", "/vets/index.html", "/vets.html"})
-//    public String listVets(Model model){
-//
-////        model.addAttribute("vets", vetService.findAll());
-//
-//        return "vets/index";
-//    }
-//
-////    @GetMapping("/api/vets")
-////    public @ResponseBody Set<Vet> getVetsJson(){
-////
-////        return vetService.findAll();
-////    }
-//}
